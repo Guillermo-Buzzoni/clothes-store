f7319a383d8266ba44d99a2e5c1c8c269f2d7a22
Guillermo-Buzzoni <guillebuzzoni@gmail.com>
2024-08-26 22:32:20 -0300

    Add products.html

--- app.py
+++ app.py
@@ -6,6 +6,31 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/products", methods=["GET"])
+def show_products():
+    return render_template("products.html") # TODO load products dynamically
+
+
+'''
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of selected stocks"""
+    if request.method == "POST":
+        user_id = session["user_id"]
+        symbol = request.form.get("symbol").upper()
+        shares_number_str = request.form.get("shares")
+
+        error, status_code = process_transaction(user_id, symbol, shares_number_str, "BUY")
+
+        if error:
+            return apology(error, status_code)
+
+        return redirect("/")
+
+    return render_template("buy.html")
+'''
+
 if __name__ == "__main__":
     app.run()
 
